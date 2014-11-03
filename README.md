2cd13a9404ac43bd1110d2ec91df8f2f070069de
Aaron Marriner <amarriner@amarriner.com>
2014-11-03 16:19:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Journey
+
+*Python script(s) to generate a novel-length (50,000 word) text for [NaNoGenMo 14](https://github.com/dariusk/NaNoGenMo-2014)*
---
+++ journey.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Script that generates a novel (!)"""
+
+import sys
+
+
+def main():
+   """Main entry point"""
+   pass
+
+
+if __name__ == '__main__':
+   sys.exit(main)
