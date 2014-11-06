2796d59c9ca1e7fd4ae722a25f339e6d497ccf02
Aaron Marriner <amarriner@amarriner.com>
2014-11-06 18:35:57 +0000

    Bit more polish, keeping track of animals/flowers, slight theme change

--- journey.py
+++ journey.py
@@ -12,6 +12,8 @@ import sys
 class Journey:
    """Class that does all the heavy lifting"""
 
+   TITLE = "FLORA AND FAUNA"
+
    CHAPTER = 0
 
    TEXT = ''
@@ -31,6 +33,14 @@ class Journey:
 
    WALK = ['walked', 'went', 'strode', 'ran', 'sprinted', 'hoofed it', 'beat cheeks', 'trekked', 'meandered', 'marched', 'shuffled', 'shambled', 'toddled', 'traipsed', 'plodded', 'strutted', 'trudged', 'paraded', 'hiked', 'ambled', 'sauntered', 'stepped']
 
+   TASTES = ['awful', 'delicious', 'terrible', 'like chicken', 'okay', 'bad', 'good']
+
+   FLOWERS = []
+   TEMP_FLOWERS = []
+
+   ANIMALS = []
+   TEMP_ANIMALS = []
+
    # -------------------------------------------------------------------------------------------------------------
    def __init__(self):
       """Constructor"""
@@ -41,7 +51,7 @@ class Journey:
    def load_json(self):
       """Pulls various corpora and loads them into the JSON object"""
 
-      f = open('corpora/data/flowers/flowers.json')
+      f = open('corpora/data/plants/flowers.json')
       self.JSON['flowers'] = json.loads(f.read().lower())['flowers']
       f.close()
 
@@ -67,6 +77,8 @@ class Journey:
 
       self.MAZE = []
       self.TEMP = ''
+      self.TEMP_FLOWERS = []
+      self.TEMP_ANIMALS = []
 
       self.DIR_INDEX = random.randrange(2)
 
@@ -194,6 +206,8 @@ class Journey:
 
       stack = []
       self.TEMP = ''
+      self.TEMP_FLOWERS = []
+      self.TEMP_ANIMALS = []
 
       # Pick a start point at random
       i = random.randrange(self.MAZE_SIZE)
@@ -227,10 +241,44 @@ class Journey:
                
                self.TEXT += "~~~ CHAPTER " + numerals(str(self.CHAPTER)).upper() + " ~~~\n"
                self.TEXT += self.TEMP
-               self.TEXT += "Then I " + random.choice(self.WALK) + " down a flight of stairs.\n"
+               self.TEXT += "Then I " + random.choice(self.WALK) + " down a flight of stairs. "
+
+               self.FLOWERS += self.TEMP_FLOWERS
+               self.ANIMALS += self.TEMP_ANIMALS
+
+               # Check to see if any animals stopped following the narrator
+               self.TEXT += self.unfollow()
+
+               if self.ANIMALS:
+                  self.TEXT += "So far " + numerals(len(self.ANIMALS)) + " animal"
+
+                  if len(self.ANIMALS) > 1:
+                     self.TEXT += "s were following me"
+                  else:
+                     self.TEXT += " was following me"
+
+                  if self.FLOWERS:
+                     self.TEXT += ", and "
+
+                  else:
+                     self.TEXT += ". "
+
+               if self.FLOWERS:
+                  if not self.ANIMALS:
+                     self.TEXT += "So far "
+
+                  self.TEXT += "I held " + numerals(len(self.FLOWERS)) + " flower"
+
+                  if len(self.FLOWERS) > 1:
+                     self.TEXT += "s"
+
+                  self.TEXT += ". "
+
+               self.TEXT += "\n"
 
                self.FOUND_EXIT = True
 
+               logging.info('--- CHAPTER ' + str(self.CHAPTER) + ' ---')
                logging.info('Found the exit to the maze at (' + str(i) + ',' + str(j) + ')')
                logging.info('Total steps: ' + str(total))
                stack = []
@@ -246,13 +294,45 @@ class Journey:
 
                # Is there a flower here?
                if random.randrange(100) < 5:
-                  self.TEMP += "There was a beautiful " + random.choice(self.JSON['colors'])['color'] + " "
-                  self.TEMP += singularize(random.choice(self.JSON['flowers'])) + " there. "
-                  self.TEMP += "It smelled like " + pluralize(random.choice(self.JSON['fruits'])) + ".\n"
+                  color  = random.choice(self.JSON['colors'])['color']
+                  flower = singularize(random.choice(self.JSON['flowers']))
+
+                  self.TEMP += "There was a beautiful " + color + " " + flower + " there. "
+                  self.TEMP += "It smelled like " + pluralize(random.choice(self.JSON['fruits'])) + "."
+
+                  # Is the narrator keeping this flower?
+                  if random.randrange(100) < 10:
+                     self.TEMP += " I picked it"
+
+                     if self.TEMP_FLOWERS:
+                        self.TEMP += " and added it to the rest of my bouquet"
+                     self.TEMP += "."
+
+                     self.TEMP_FLOWERS.append(referenced(color + " " + flower))
+
+                  # Does the narrator eat this flower instead?
+                  elif random.randrange(100) < 5:
+                     self.TEMP += " For some reason I ate it. It tasted " + random.choice(self.TASTES) + "."
+
+                  self.TEMP += "\n"
+                  then = False
+
                # Or is there an animal here?
                elif random.randrange(100) < 5:
-                  self.TEMP += "There was " + referenced(random.choice(self.JSON['animals'])) + " there. "
-                  self.TEMP += "I named it " + random.choice(self.JSON['names']) + ".\n"
+                  animal = random.choice(self.JSON['animals'])
+                  name = random.choice(self.JSON['names'])
+
+                  self.TEMP += "There was " + referenced(animal) + " there. "
+                  self.TEMP += "I named it " + name + "."
+
+                  # Did the animal follow the narrator?
+                  if random.randrange(100) < 10:
+                     self.TEMP_ANIMALS.append(name + " the " + animal)
+                     self.TEMP += " It started following me."
+
+                  self.TEMP += "\n"
+
+                  then = False
 
                self.MAZE[i][j]['v'] = 1
                self.MAZE[i][j][next[2]] = 1
@@ -272,10 +352,101 @@ class Journey:
             square = stack.pop()
 
    # -------------------------------------------------------------------------------------------------------------
+   def unfollow(self):
+      """Loop through the following animals list and randomly pick some to remove"""
+
+      unfollowed = []
+      for a in self.ANIMALS:
+         if random.randrange(100) < 3:
+            unfollowed.append(a)
+            self.ANIMALS.remove(a)
+
+      temp = ""
+      for a in unfollowed:
+         if a == unfollowed[-1] and len(unfollowed) > 1:
+            temp += ", and "
+         elif a != unfollowed[0]:
+            temp += ", "
+
+         temp += a
+
+      if temp:
+         temp += " stopped following me for some reason.\n"
+
+      return temp
+
+   # -------------------------------------------------------------------------------------------------------------
    def count_words(self):
       """Returns the current number of words in the text"""
 
-      return len(re.split(r'[^0-9A-Za-z]+', self.TEXT))
+      return len(re.split(r'[^0-9A-Za-z]+', self.full_text()))
+
+   # -------------------------------------------------------------------------------------------------------------
+   def get_prologue(self):
+      """Returns the assembled prologue"""
+
+      temp  = "~~~ PROLOGUE ~~~\nI am a botanist and a zoologist. "
+      temp += "I came to this complex of mazes to catalog flora and fauna. "
+      temp += "What follows is the journal of my travails.\n"
+
+      return temp
+
+   # -------------------------------------------------------------------------------------------------------------
+   def get_afterword(self):
+      """Returns the assembled afterword"""
+
+      temp  = "\n~~~ AFTERWORD ~~~\n"
+      temp += "I finally made my way through every floor and out into the sunlight. "
+      temp += "Below is a list of some of the things I cataloged inside this massive place.\n"
+      temp += self.get_animals()
+      temp += self.get_flowers()
+      temp += "\nTHE END\n"
+
+      return temp
+
+   # -------------------------------------------------------------------------------------------------------------
+   def get_animals(self):
+      """Returns a textual representation of following animals"""
+
+      temp = ""
+      if self.ANIMALS:
+         temp += "I was followed by: "
+
+         for a in self.ANIMALS:
+            if a == self.ANIMALS[-1] and len(self.ANIMALS) > 1:
+               temp += ", and "
+
+            elif a != self.ANIMALS[0]: 
+               temp += ", "
+
+            temp += a
+
+      return temp + ".\n"
+
+   # -------------------------------------------------------------------------------------------------------------
+   def get_flowers(self):
+      """Returns a textual representation of carried flowers"""
+
+      temp = ""
+      if self.FLOWERS:
+         temp += "I left with a beautiful bouquet of flowers that contained: "
+
+         for f in self.FLOWERS:
+            if f == self.FLOWERS[-1] and len(self.FLOWERS) > 1:
+               temp += ", and "
+
+            elif f != self.FLOWERS[0]:
+               temp += ", "
+
+            temp += f
+
+      return temp + ".\n"
+
+   # -------------------------------------------------------------------------------------------------------------
+   def full_text(self):
+      """Returns the full text of the novel as a string"""
+
+      return self.TITLE + "\n" + self.get_prologue() + self.TEXT + self.get_afterword()
 
    # -------------------------------------------------------------------------------------------------------------
    def write_text(self):
@@ -283,7 +454,7 @@ class Journey:
 
       text = ""
 
-      for line in self.TEXT.split("\n"):
+      for line in self.full_text().split("\n"):
          
          if len(line) > 80:
 
@@ -309,9 +480,6 @@ class Journey:
 
          else:
             text += line + "\n\n"
-
-      text  = "~~~ FORWARD ~~~\n\nI am an adventurer. I came to this complex of mazes to seek glory.\nWhat follows is the journal of my travails.\n\n" + text.strip()
-      text += "\n\n~~~ AFTERWARD ~~~\n\nI sought glory, and I found it at the bottom of this massive place.\n\nTHE END\n"
 
       f = open('journey.txt', 'w')
       f.write(text)
